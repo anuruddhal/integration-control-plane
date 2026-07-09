0199527fb499d0d9d0b5c7b4d6d68b95b75e524d
dilhasha <dilhasha.uom@gmail.com>
2026-07-09 11:30:13 +0530

    Add type changes for inbound ep

--- icp_server/resources/db/init-scripts/h2_init.sql
+++ icp_server/resources/db/init-scripts/h2_init.sql
@@ -978,7 +978,7 @@ CREATE TABLE mi_inbound_endpoint_artifacts (
     runtime_id CHAR(36) NOT NULL,
     inbound_name VARCHAR(200) NOT NULL,
     artifact_id CHAR(36) NOT NULL UNIQUE,
-    protocol VARCHAR(50) NOT NULL,
+    protocol VARCHAR(50),
     sequence VARCHAR(200),
     statistics VARCHAR(20),
     on_error VARCHAR(200),
